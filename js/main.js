3402977a51b08f0e6eadac350810e1dbeff44cc5
Silentius222 <79799875+Silentius222@users.noreply.github.com>
2022-06-15 17:33:14 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
 document.querySelector("button").onclick = function(){
     let nazwa1 = document.getElementById("name");
-    console.log(nazwa1.value);
     let imie = document.querySelector("#imie");
 
 
     let nazwa2 = document.getElementById("surname");
-    console.log(nazwa2.value);
     let nazwisko = document.querySelector("#nazwisko");
 
     let nazwa3 = document.getElementById("phone");
-    console.log(nazwa3.value);
     let telefon = document.querySelector("#telefon");
 
-    imie.textContent = telefon;
-    nazwa2.textContent = nazwisko;
-    nazwa3.textContent = telefon;
+    nazwa1.textContent = imie.value;
+    nazwa2.textContent = nazwisko.value;
+    nazwa3.textContent = telefon.value;
 
     console.log(imie.value);
     console.log(nazwisko.value);
